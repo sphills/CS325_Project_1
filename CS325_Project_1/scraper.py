7464b718a871c444d443b8ff06c5743111cf142f
Sage Phillips <sage242@hotmail.com>
2024-02-12 11:44:22 -0600

    Added a print to the commandline so the user knows what title to look for their summary in.

--- CS325_Project_1/scraper.py
+++ CS325_Project_1/scraper.py
@@ -4,17 +4,10 @@ from bs4 import BeautifulSoup
 
 parser = argparse.ArgumentParser(description='Scrape data from a given article from the BBC news agency.')
 parser.add_argument('-a', '--article_URL', type=str, default="https://www.bbc.com/future/article/20231130-climate-crisis-the-15c-global-warming-threshold-explained", help="The URL of the article you'd like summarized.")
-#url = 'https://www.bbc.com/future/article/20231130-climate-crisis-the-15c-global-warming-threshold-explained'
-#url = 'https://www.bbc.com/future/article/20240206-craftivism-the-introverts-guide-to-gentle-protest-for-climate'
-#url = 'https://www.bbc.com/future/article/20240131-how-planting-trees-is-bringing-clean-water-to-a-tropical-nation'
-#url = 'https://www.bbc.com/news/uk-england-essex-68274304'
-#url = 'https://www.bbc.com/news/uk-england-lincolnshire-68272816'
+
 args = parser.parse_args()
 url = args.article_URL
 page = requests.get(url)
-'''start = int()
-end = int()'''
-print(url)
 soup = BeautifulSoup(page.content, 'lxml')
 
 articles = soup.findAll('section', {'class': 'sc-4e574cd-0'})
@@ -24,15 +17,12 @@ for index, article in enumerate(articles):
     if index < (len(articles)-4):             # The bottom four lines of a given article are a horizontal spacer and the author summary
         lines.append(article.get_text())
 
-#file_name = "BBC Climate Crisis Summary.txt"
-#file_name = "Gentle Protest Summary.txt"
-#file_name = "Planting Trees Summary.txt"
-#file_name = "Littering Summary.txt"
-#file_name = "Solar Farm Summary.txt"
 file_name = (url[(url.rfind('/') + 10):len(url)].replace('-', ' ').title() + " Summary.txt")
 file = open(file_name, "w")
 
 for line in lines:
     file.write(line.replace('\u200a', ' ') + "\n")
+
+print("Your article was successfully summarized and written to the file\"", file_name + "\" in the current directory")
 
 file.close()
