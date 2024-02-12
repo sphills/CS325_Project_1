b91df18f12ab4a142f92976c09c820d4e2b2276e
Sage Phillips <sage242@hotmail.com>
2024-02-12 15:01:49 -0600

    Added in reading from input article url file.

--- CS325_Project_1/scraper.py
+++ CS325_Project_1/scraper.py
@@ -7,23 +7,33 @@ parser.add_argument('-i', '--check_input_file', type=bool, default=True, help="W
 parser.add_argument('-a', '--article_URL', type=str, help="The URL of the article you'd like summarized.")
 
 args = parser.parse_args()
+check_input_file = args.check_input_file
 url = args.article_URL
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'lxml')
 
-articles = soup.findAll('section', {'class': 'sc-4e574cd-0'})
-lines = []
+def scrape_url(url: str):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'lxml')
 
-for index, article in enumerate(articles):
-    if index < (len(articles)-4):             # The bottom four lines of a given article are a horizontal spacer and the author summary
-        lines.append(article.get_text())
+    articles = soup.findAll('section', {'class': 'sc-4e574cd-0'})
+    lines = []
 
-file_name = (url[(url.rfind('/') + 10):len(url)].replace('-', ' ').title() + " Summary.txt")
-file = open(file_name, "w")
+    for index, article in enumerate(articles):
+        if index < (len(articles)-4):             # The bottom four lines of a given article are a horizontal spacer and the author summary
+            lines.append(article.get_text())
 
-for line in lines:
-    file.write(line.replace('\u200a', ' ') + "\n")
+    file_name = (url[(url.rfind('/') + 10):len(url)].replace('-', ' ').title() + " Summary.txt")
+    file = open(file_name, "w")
 
-print("Your article was successfully summarized and written to the file", "\"" + file_name + "\"", "in the current directory")
+    for line in lines:
+        file.write(line.replace('\u200a', ' ') + "\n")
 
-file.close()
+    print("Your article was successfully summarized and written to the file", "\"" + file_name + "\"", "in the current directory")
+
+    file.close()
+
+if (check_input_file):
+    input_file = open("input_articles.txt", "r")
+    for line in input_file:
+        scrape_url(line.strip())
+elif (check_input_file) and (url is not None):
+    scrape_url(url)
